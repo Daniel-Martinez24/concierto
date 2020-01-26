e18de948417377343a370a5b6481940dd2ccd58a
Daniel Martinez Diaz <dan24diaz@gmail.com>
2020-01-26 12:32:28 -0600

    añadido navbar desktop

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,30 +1,26 @@
 <template>
   <div id="navBar" class="text">
     <ul id="izquiera">
+      <li>
+        <el-button type="text">
+          <img src="~/static/logo.png" alt="">
+        </el-button>
+      </li>
       <li><el-button id="irMain" round><p>MT25 PRODUCCIONES</p></el-button></li>
     </ul>
     <ul id="derecha">
-      <li><el-button type="text">ACCEDE A TU CUENTA</el-button></li>
+      <li>
+        <el-button type="text">
+          <p>
+            ACCEDE A TU CUENTA
+          </p>
+        </el-button>
+      </li>
     </ul>
   </div>
 </template>
 
 <style scoped>
-  #irMain {
-      height: 1vh;
-      width: 100%;
-      margin: -10%;
-      color: #00279B;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-  }
-  #irMain p {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin: -4%;
-  }
   .text {
       Font-Family: fantasy;
       Font-Style: Bold;
@@ -55,5 +51,40 @@
   }
   #derecha li{
       float:left;
+      align-items: center;
+  }
+  #derecha li p{
+      color: white;
+  }
+  #izquiera {
+      margin: 0%;
+      padding: 0%;
+  }
+  #izquiera li {
+      float:left;
+      align-items: center;
+  }
+  #izquiera li img{
+      height: 7vh;
+      width: 7vh;
+      margin: 0%;
+      padding: 0%;
+  }
+  #irMain {
+      height: 1vh;
+      width: auto;
+      margin-top: 11%;
+
+      margin-left: -15%;
+      color: #00279B;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+  #irMain p {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: -4%;
   }
 </style>
